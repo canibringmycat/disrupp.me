bb62a75947e5af84548705ff5fc4e7efbe7160ac
Sam Choi <sam.choi@berkeley.edu>
2016-07-17 00:18:17 -0700

    basic function template for scraping startup names

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,8 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+from urllib2 import urlopen
 
 # Websites to scrape names from
 # http://yclist.com/
 # http://angelpad.org/
 # http://www.techstars.com/companies/
+
+def get_startup_names(site_url):
+	startup_names = []
+	html = urlopen(site_url).read()
+	# soup = BeautifulSoup(html, "lxml")
+	# container = soup.find("..., ...")
+	# startup_names = []
+    return startup_names
+
