1d1faa2635a300e7bd8e58c996fd7022cafd7762
Antares Chen <antares21chen@gmail.com>
2016-08-19 12:41:26 -0700

    updated scraper

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,39 +1,58 @@
 from bs4 import BeautifulSoup
 import requests
-import re
-from urllib2 import urlopen
 
-# Websites to scrape names from
-# http://yclist.com/
-# http://angelpad.org/
-# http://www.techstars.com/companies/
+class Scraper(object):
 
-# def get_startup_names(site_url):
-# 	startup_names = []
-# 	html = urlopen(site_url).read()
-# 	# soup = BeautifulSoup(html, "lxml")
-# 	# container = soup.find("..., ...")
-# 	# startup_names = []
-#     return startup_snames
+	def __init__(self, url):
+		""" Initializes the Scraper to scrape from the provided url. """
+		self.url = url
 
-websites = ["http://yclist.com/", "http://angelpad.org/", "http://www.techstars.com/companies/"]
+	def soupify(self):
+		""" Returns a BeautifulSoup instance on the html for self.url """
+		response = requests.get(self.url)
+		soup = BeautifulSoup(response.content, "html.parser")
+		return soup
 
-complete_directory = []
-
-for website in websites:
-	r = urlopen(website).read()
-	soup = BeautifulSoup(r)
-	print(soup)
-	# curr_list = get_startup_names(website)
-	# complete_directory.append(curr_list)
+	def dump(self, names, out_file):
+		with open(out_file, 'w') as f:
+			for elem in names:
+				f.write("{0}\n".format(elem))
 
 
-for start_up in complete_directory:
-	print(start_up)
+class YCScraper(Scraper):
+
+	def __init__(self):
+		""" Initializes the Scraper to scrape YCombinator. """
+		url = "http://yclist.com/"
+		super(YCScraper, self).__init__(url)
+
+	def parse_startups(self):
+		""" LOL this is such a hack. """
+		soup = self.soupify()
+		results = soup.find_all(lambda tag : tag.name == 'tr' and tag.has_attr('class'))
+		names = []
+		for elem in results:
+			children = [c for c in elem.children]
+			names += children[3].contents
+		return names
+
+class AngelPadScraper(Scraper):
+
+	def __init__(self):
+		url = "http://angelpad.org/"
+		super(AngelPadScraper, self).__init__(url)
+
+	def parse_startups(self):
+		# TODO implement
+		pass
 
 
 
+class TechStarsScraper(Scraper):
+	def __init__(self):
+		url = "http://www.techstars.com/companies/"
+		super(TechStarsScraper, self).__init__(url)
 
-
-
-
+	def parse_startups(self):
+		# TODO implement
+		pass
