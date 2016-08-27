5b126f83e5f38fb32d7fe36ea271a7181fd8a9c9
Sam Choi <sam.choi@berkeley.edu>
2016-08-26 21:00:59 -0700

    pulled changes:

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -20,16 +20,37 @@ websites = ["http://yclist.com/", "http://angelpad.org/", "http://www.techstars.
 
 complete_directory = []
 
-for website in websites:
-	r = urlopen(website).read()
-	soup = BeautifulSoup(r)
-	print(soup)
+# for website in websites:
+# 	r = urlopen(website).read()
+# 	soup = BeautifulSoup(r)
+# 	print(soup)
 	# curr_list = get_startup_names(website)
 	# complete_directory.append(curr_list)
 
+# YCombinator List
+content = urlopen(websites[0]).read()
+soup = BeautifulSoup(content, "lxml")
+gdata = soup.find_all("td")
+lst = []
+# prev_item = "not_a_link"
 
-for start_up in complete_directory:
-	print(start_up)
+print(soup.get_text())
+print(type(soup.get_text()))
+print(soup.td.contents.prettify())
+
+	# item_string = item_string.rstrip('\n')
+	# item_string = item_string.strip()
+
+	# lst.append(item.text)
+	# if (item.text[:2] == "htt"):
+	# 	print(item.text)
+	# 	lst.append(prev_item)
+	# prev_item = item.text
+
+
+
+# for start_up in complete_directory:
+# 	print(start_up)
 
 
 
