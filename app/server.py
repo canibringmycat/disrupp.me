65fe0fa234ac310936b64f981f356419e448d485
Antares Chen <antares21chen@gmail.com>
2016-08-27 19:05:51 -0700

    The app now routes index

--- app/server.py
+++ app/server.py
@@ -1,5 +1,14 @@
+from flask import render_template, request
 from app import app
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Hello, World!"
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        pass
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
