b9a71a4736b7716673f58680fa009b405bce1575
Sam Choi <sam.choi@berkeley.edu>
2016-08-28 18:59:25 -0700

    back 2 basics

--- app/disrupp.py
+++ app/disrupp.py
@@ -1,4 +1,5 @@
 """ Contains all code related to the name-generation model.
+	- generates industry-specific Markov models
 
 Created: August 27, 2016
 """
@@ -8,3 +9,11 @@ import utils
 class StartupGenerator(object):
 	def __init__(self):
 		# write code here
+
+class MarkovModel(object):
+	def __init__(self, order=3, t_data):
+		training_set = []
+		start_sequence = "_" * order
+		# initialize model here
+		for i in (len(t_data) - 1):
+			training_set.append(start_sequence + t_data[i])
