c9205d4656ee350e7f76edf77fa5b4cd913d0cad
Sam Choi <sam.choi@berkeley.edu>
2016-08-29 02:08:40 -0700

    model work...

--- app/disrupp.py
+++ app/disrupp.py
@@ -9,15 +9,18 @@ import random
 import json
 
 class StartupGenerator(object):
-	def __init__(self, order=3, json_file, industry):
+	def __init__(self, order=3, json_file, firstname, lastname, industry):
 		# initialize startup generator here
 		with open(json_file) as json_data:
 			data = json.load(json_data)
+		# json file
+		# --> key: industry name
+		# --> value: list of startup names
 		self.model = MarkovModel(order, data[industry])
 
 	def generate(self):
 		self.model.start()
-		return self.model.generate()
+		return self.model.generate(firstname, lastname)
 
 
 class MarkovModel(object):
@@ -31,12 +34,38 @@ class MarkovModel(object):
 	def start(self):
 		self.prefix = "_" * self.order
 		# initialize model here
-		for i in (len(self.data) - 1):
+		for i in range(len(self.data)):
 			self.training_set.append(self.prefix + self.data[i] + self.terminator)
 
-	def generate(self, firstname, lastname, industry):
+	def generate(self, firstname, lastname):
 		# Random Number Generator
 		random.seed(firstname + lastname)
 		print("printing random: " + random.random())
+
+
+	def add_to_dict(dictionary, elem):
+		if elem in dictionary:
+			dictionary[elem] += 1
+		else:
+			dictionary[elem] = 1
+
+	def find_next(prefix, word):
+		if prefix not in word:
+			return []
+		start = word.index(prefix)
+		return [word[start + 3]] + find_next(prefix, word[(start + 1):])
+
+
+	# to be placed somewhere in the generate function
+	seen = {'total': 0} # next letters and total occurences - to calculate probability
+
+	for word in self.training_set:
+		if self.prefix in word:
+			next_list = find_next(self.prefix, word)
+			for elem in next_list:
+				add_to_dict(seen, elem)
+				seen['total'] += 1
+
+
 
 
